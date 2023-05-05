d2d46b154c9a956280d82b4868717a5a3280947d
Dmitriy Chebotarev <127520783+Dimaskop@users.noreply.github.com>
2023-05-05 18:17:43 +0300

    pergr funkcii

--- Human.cpp
+++ Human.cpp
@@ -4,14 +4,22 @@ using namespace std;
 
 class Human {
 public:
+	string name;
+	int age = 0;
 
-
-
+	void add_age (int x){
+		age += x;
+	}
+	void add_age() {
+		age += 1;
+	}
 };
+
 int main()
 {
-
-
-
+	Human h = { "Alex",50 };
+	h.add_age();
+	h.add_age(2);
+	cout << h.age;
 }
 
