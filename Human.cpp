8b39932bb09c884bbd3ff50b04470ee2a00fb524
Dmitriy Chebotarev <127520783+Dimaskop@users.noreply.github.com>
2023-05-05 18:49:28 +0300

    friend human

--- Human.cpp
+++ Human.cpp
@@ -18,12 +18,17 @@ public:
 		other.age += val;
 		return other;
 	}
+	friend Human& operator + (int val, Human & h);
 };
-
+	Human& operator + (int val, Human& h) {
+		Human other = h;
+		other.age += val;
+		return other;
+	}
 int main()
 {
 	Human h = { "Alex",50 };
-	h = h + 1;
+	h = 5 + h;
 	
 	cout << h.age;
 }
